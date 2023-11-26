e93ecd0eb6f4a72dff868075130df08e6a520d55
CorinaOxani <corina.oxani@student.upt.ro>
2023-11-26 15:35:53 +0200

    added sending confirmation mail functionality

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
+import { Link, useNavigate } from 'react-router-dom';
 import { auth, fs } from '../Config/config';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import { Navbar } from "./Navbar";
 
 export const Signup = () => {
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
   const [fullName, setFullname] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -17,13 +17,13 @@ export const Signup = () => {
   const validatePassword = (password) => {
     const hasUpperCase = /[A-Z]/.test(password);
     const hasNumber = /\d/.test(password);
-    const isLongEnough = password.length >= 8; // example for minimum 8 characters
+    const isLongEnough = password.length >= 8;
   
     if (!hasUpperCase) return "Password must include at least one uppercase letter.";
     if (!hasNumber) return "Password must include at least one number.";
     if (!isLongEnough) return "Password must be at least 8 characters long.";
     
-    return ""; // returns an empty string if all conditions are met
+    return "";
   };
 
   const handlePasswordChange = (e) => {
@@ -31,7 +31,7 @@ export const Signup = () => {
     setPassword(newPassword);
   
     const validationMessage = validatePassword(newPassword);
-    setErrorMsg(validationMessage); // Update the error message based on validation
+    setErrorMsg(validationMessage);
   };
 
   const handleSignup = async (e) => {
@@ -40,22 +40,22 @@ export const Signup = () => {
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-  
+
+      // Send verification email
+      await sendEmailVerification(user);
+
       const userData = {
         Name: fullName,
         Email: email,
         Password: password,
       };
   
-      // Check if the "admin" checkbox is selected
       if (isAdmin) {
-        // Save user data in the "admin" database
         const adminUserDocRef = doc(fs, 'AdminUserData', user.uid);
         await setDoc(adminUserDocRef, userData);
         const userDocRef = doc(fs, 'SignedUpUsersData', user.uid);
         await setDoc(userDocRef, userData);
       } else {
-        // Save user data in the "users" database
         const userDocRef = doc(fs, 'SignedUpUsersData', user.uid);
         await setDoc(userDocRef, userData);
       }
@@ -64,11 +64,8 @@ export const Signup = () => {
       setEmail('');
       setPassword('');
       setErrorMsg('');
-      setSuccessMsg(
-        'Account created successfully. You will be redirected automatically to Log In form.'
-      );
+      setSuccessMsg('Account created successfully. Check your email for verification. You will be redirected automatically to Log In form.');
   
-      // Automatically redirect to the login page after a delay
       setTimeout(() => {
         setSuccessMsg('');
         navigate('/login');
@@ -77,69 +74,68 @@ export const Signup = () => {
       setErrorMsg(error.message);
     }
   };
-  
 
   return (
     <>
       <Navbar />
-    <div className="container-form">
-      <br />
-      <h2>Sign up</h2>
-      <hr />
-      {successMSG && (
-        <div className="success-msg">{successMSG}</div>
-      )}
-      <form autoComplete="off" className="form-group" onSubmit={handleSignup}>
-        <label>Full Name</label>
-        <input
-          type="text"
-          className="form-control"
-          required
-          onChange={(e) => setFullname(e.target.value)}
-          value={fullName}
-        />
+      <div className="container-form">
         <br />
-        <label>Email</label>
-        <input
-          type="email"
-          className="form-control"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <br />
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          required
-          onChange={handlePasswordChange}
-          value={password}
-        />
-        <br />
-        <label style={{ display: 'flex', alignItems: 'center', gap: '10px', fontSize: '16px', color: '#333', cursor: 'pointer' }}>
-          Admin Role
+        <h2>Sign up</h2>
+        <hr />
+        {successMSG && (
+          <div className="success-msg">{successMSG}</div>
+        )}
+        <form autoComplete="off" className="form-group" onSubmit={handleSignup}>
+          <label>Full Name</label>
           <input
-            type="checkbox"
-            onChange={(e) => setIsAdmin(e.target.checked)}
-            style={{ margin: '0', width: '20px', height: '20px' }}
+            type="text"
+            className="form-control"
+            required
+            onChange={(e) => setFullname(e.target.value)}
+            value={fullName}
           />
-        </label>
-        <div className="btn-box">
-          <button type="submit" className="RightsideNavlink">
-            Sign Up
-          </button>
           <br />
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
           <br />
-          <span>
-            You already have an account? <Link to="/login">Log In</Link>
-          </span>
-        </div>
-      </form>
-      {errorMSG && (
-        <div className="error-msg">{errorMSG}</div>
-      )}
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            required
+            onChange={handlePasswordChange}
+            value={password}
+          />
+          <br />
+          <label style={{ display: 'flex', alignItems: 'center', gap: '10px', fontSize: '16px', color: '#333', cursor: 'pointer' }}>
+            Admin Role
+            <input
+              type="checkbox"
+              onChange={(e) => setIsAdmin(e.target.checked)}
+              style={{ margin: '0', width: '20px', height: '20px' }}
+            />
+          </label>
+          <div className="btn-box">
+            <button type="submit" className="RightsideNavlink">
+              Sign Up
+            </button>
+            <br />
+            <br />
+            <span>
+              You already have an account? <Link to="/login">Log In</Link>
+            </span>
+          </div>
+        </form>
+        {errorMSG && (
+          <div className="error-msg">{errorMSG}</div>
+        )}
       </div>
-      </>
+    </>
   );
 };
